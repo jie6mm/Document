0b11f8821e0c8b0919b45f8114755554cd1bcc33
mengshi礞石) <mengshi@fudata.com>
2021-06-04 17:54:21 +0800

    gitlab & github

--- .deployment/jenkins.sh
+++ .deployment/jenkins.sh
@@ -6,12 +6,10 @@
 # Create-Time: 2021-06-04 11:42:00
 # Update-Time: -
 #*********************************************
-echo "document-jenkins.sh开始执行";
+gitbook-path=/home/root/app/gitbook/gitbook
 
-scp -r ./* root@192.168.50.210:/home/root/app/gitbook/document
+echo "document-jenkins.sh开始执行"
 
-ssh -Tq root@192.168.50.210
+scp -rp ./* root@192.168.50.210:${gitbook-path}
 
-cd /home/root/app/gitbook/document
-
-ls -l
+ssh root@192.168.50.210 "sh `${gitbook-path}`/.deployment/gitbook-restart.sh"
