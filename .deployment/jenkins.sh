85c2c56ee69f615d0217c95d197d50c937d2003a
mengshi礞石) <mengshi@fudata.com>
2021-06-04 11:46:49 +0800

    docker-jenkins.md

---
+++ .deployment/jenkins.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#*********************************************
+# Author: jie6mm
+# Title: document-jenkins.sh
+# Description: -
+# Create-Time: 2021-06-04 11:42:00
+# Update-Time: -
+#*********************************************
+echo "document-jenkins.sh开始执行";
