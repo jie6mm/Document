5b946e085e73591bf832c7f1091bfe947a6a9b4c
mengshi礞石) <mengshi@fudata.com>
2021-06-04 17:58:22 +0800

    gitlab & github

--- .deployment/jenkins.sh
+++ .deployment/jenkins.sh
@@ -6,7 +6,7 @@
 # Create-Time: 2021-06-04 11:42:00
 # Update-Time: -
 #*********************************************
-set gitbook-path=/home/root/app/gitbook/gitbook
+var gitbook-path=/home/root/app/gitbook/gitbook
 
 echo "document-jenkins.sh开始执行"
 
