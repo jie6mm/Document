9260a56ec54c2b7a4a1a39856865626fe097192b
mengshi(礞石) <mengshi@fudata.com>
2021-06-10 14:17:14 +0800

    .gitignore

--- .deployment/jenkins.sh
+++ .deployment/jenkins.sh
@@ -8,11 +8,15 @@
 #*********************************************
 echo "document-jenkins.sh开始执行"
 
-chmod a+x .deployment/gitbook-docker-restart.sh
+#chmod a+x .deployment/gitbook-docker-restart.sh
 
 ssh root@192.168.50.210 "rm -rf /home/root/app/gitbook/gitbook/.deployment/*"
 
+ssh root@192.168.50.210 "rm -rf /home/root/app/gitbook/gitbook/.gitbook.yaml"
+
 scp -rp ./.deployment root@192.168.50.210:/home/root/app/gitbook/gitbook
+
+scp -rp ./.gitbook.yaml root@192.168.50.210:/home/root/app/gitbook/gitbook
 
 ssh root@192.168.50.210 "sh /home/root/app/gitbook/gitbook/.deployment/gitbook-clean.sh"
 
